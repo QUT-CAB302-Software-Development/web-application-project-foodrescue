e05b03a7c16c120f746f1fa61a7b682108534ec4
Angelique Zantiotis <angelique.zantiotis@connect.qut.edu.au>
2023-04-18 22:26:32 +1000

    Added food listing class, listing methods and some listing tests

---
+++ demo/src/main/java/example/data/Listings.java
@@ -0,0 +1,61 @@
+package example.data;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Listings {
+
+    public List<FoodListing> allListings = new ArrayList<>();
+
+    public void addListing(FoodListing listing){
+        allListings.add(listing);
+    }
+
+    public void deleteListing(FoodListing listing){
+        allListings.remove(listing);
+    }
+
+    public List<FoodListing> dietaryFilter(List<FoodListing> allListings, String dietaryRequirement){
+        List<FoodListing> filteredListings = new ArrayList<>();
+        for(FoodListing listing: allListings){
+            if(listing.getDietaryDetails().contains(dietaryRequirement)){
+                filteredListings.add(listing);
+            }
+
+        }
+        return filteredListings;
+    }
+
+    public List<FoodListing> pickupTimeFilter(List<FoodListing> allListings, Time time){
+        List<FoodListing> filteredListings = new ArrayList<>();
+        for(FoodListing listing: allListings){
+            if(listing.getPickupTime().equals(time)){
+                filteredListings.add(listing);
+            }
+
+        }
+        return filteredListings;
+    }
+
+    public List<FoodListing> quantityFilter(List<FoodListing> allListings, Integer quantity){
+        List<FoodListing> filteredListings = new ArrayList<>();
+        for(FoodListing listing: allListings){
+            if(listing.getQuantity() >= quantity){
+                filteredListings.add(listing);
+            }
+
+        }
+        return filteredListings;
+    }
+
+    public List<String> getAllListings(){
+        List<String> listings = new ArrayList<String>();
+        for(FoodListing listing : allListings){
+            System.out.println(listing.getTitle());
+        }
+        return listings;
+    }
+
+}
