6947841852ee9ecb8a7060609bcd8fa840890546
Angelique Zantiotis <angelique.zantiotis@connect.qut.edu.au>
2023-04-25 16:21:45 +1000

    created new post controller and HTML/CSS files

---
+++ FoodRescue/src/main/java/example/application/controllers/NewPostController.java
@@ -0,0 +1,14 @@
+package example.application.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NewPostController {
+        @GetMapping("/NewPost")
+        public String home() {
+            return "new-post";
+        }
+
+}
+
