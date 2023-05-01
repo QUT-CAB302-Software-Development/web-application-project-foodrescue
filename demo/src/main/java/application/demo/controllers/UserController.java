864676ff6243591899032089d3096e49b885df54
Sakshee Tosniwal <72787224+sakshee29@users.noreply.github.com>
2023-05-02 02:22:10 +1000

    Adding new demo project

---
+++ demo/src/main/java/application/demo/controllers/UserController.java
@@ -0,0 +1,15 @@
+package application.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    @GetMapping
+    @ResponseBody
+    public String homepage(){
+        return "Hello World";
+    }
+}
---
+++ demo/src/main/java/application/demo/data/UserRepository.java
@@ -0,0 +1,9 @@
+package application.demo.data;
+
+import application.demo.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
