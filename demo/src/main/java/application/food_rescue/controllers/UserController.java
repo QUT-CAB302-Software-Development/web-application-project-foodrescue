332bab3b9d78cdcd521522d1336a4cb62ed004be
Sakshee Tosniwal <72787224+sakshee29@users.noreply.github.com>
2023-05-03 09:18:33 +1000

    Fixed remaining package names

--- demo/src/main/java/application/food_rescue/controllers/UserController.java
+++ demo/src/main/java/application/food_rescue/controllers/UserController.java
@@ -1,4 +1,4 @@
-package application.demo.controllers;
+package application.food_rescue.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- demo/src/main/java/application/food_rescue/controllers/WebConfig.java
+++ demo/src/main/java/application/food_rescue/controllers/WebConfig.java
@@ -1,4 +1,4 @@
-package application.demo.controllers;
+package application.food_rescue.controllers;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
