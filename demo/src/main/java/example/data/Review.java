0a05cdf1fe0a92023001133dccebff16143b46e7
Bennette Benoy <bennette.benoy@outlook.com>
2023-05-03 08:08:15 +1000

    Add getters and setters to private variables

--- demo/src/main/java/example/data/Review.java
+++ demo/src/main/java/example/data/Review.java
@@ -12,26 +12,8 @@ public class Review {
     private int rating;
 
 
-    /**
-     * Method 5
-     * Review constructor that creates a review only when ratings are valid and the review description
-     * in not null
-     */
-    public Review(String title, String description, int rating) throws InvalidReviewException {
-        if (title == null || description
-                == null) {
-            throw new InvalidReviewException("Title and description cannot be null");
-        }
-        if (rating < 1 || rating > 5) {
-            throw new InvalidReviewException("Invalid rating");
-        }
-        this.title = title;
-        this.description = description;
-        this.rating = rating;
-    }
 
-
-
+    private long userId;
     public String getTitle() {
         return title;
     }
@@ -55,6 +37,35 @@ public class Review {
     public void setRating(int rating) {
         this.rating = rating;
     }
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+
+
+    /**
+     * Method 5
+     * Review constructor that creates a review only when ratings are valid and the review description
+     * in not null
+     */
+    public Review(String title, String description, int rating) throws InvalidReviewException {
+        if (title == null || description
+                == null) {
+            throw new InvalidReviewException("Title and description cannot be null");
+        }
+        if (rating < 1 || rating > 5) {
+            throw new InvalidReviewException("Invalid rating");
+        }
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+    }
+
+
 
 
 }
