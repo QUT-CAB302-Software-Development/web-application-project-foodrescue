9121296491d3ca34395fd8895b9f10be2a018384
Sakshee Tosniwal <72787224+sakshee29@users.noreply.github.com>
2023-05-03 09:11:58 +1000

    Code refactored to list correct package names

--- demo/src/main/java/application/food_rescue/FoodRescueApplication.java
+++ demo/src/main/java/application/food_rescue/FoodRescueApplication.java
@@ -1,4 +1,4 @@
-package application.demo;
+package application.food_rescue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
