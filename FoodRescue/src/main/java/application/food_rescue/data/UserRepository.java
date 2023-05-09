852c711a75d3c05dc5f8754e248b7327029c5de0
Alif Papp <nitrojuice051@gmail.com>
2023-05-09 11:12:52 +1000

    Delete FoodRescue directory

--- FoodRescue/src/main/java/application/food_rescue/data/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package application.food_rescue.data;
-
-import application.food_rescue.models.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
--- FoodRescue/src/main/java/example/data/Exceptions/InvalidReviewException.java
+++
@@ -1,7 +0,0 @@
-package example.data.Exceptions;
-
-public class InvalidReviewException extends Exception {
-    public InvalidReviewException(String message) {
-        super(message);
-    }
-}
