1b5297cca662964f30babc389d06b6d9aeb32ba1
Sakshee Tosniwal <72787224+sakshee29@users.noreply.github.com>
2023-05-10 09:24:22 +1000

    fake push

--- demo/src/main/java/application/food_rescue/data/BusinessPageRepository.java
+++ demo/src/main/java/application/food_rescue/data/BusinessPageRepository.java
@@ -3,7 +3,7 @@ package application.food_rescue.data;
 import application.food_rescue.models.BusinessPage;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+//Add a comment
 @Repository
 public interface BusinessPageRepository extends CrudRepository<BusinessPage, Integer> {
 }
